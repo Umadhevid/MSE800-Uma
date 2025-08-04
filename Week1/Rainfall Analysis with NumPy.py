10fb8b2e83d33153ef012de52880b1ef01823f17
umadhevid <umadhevid@gmail.com>
2025-08-04 18:46:30 +1200

    Update Rainfall Analysis with NumPy.py

--- Week1/Rainfall Analysis with NumPy.py
+++ Week1/Rainfall Analysis with NumPy.py
@@ -16,12 +16,12 @@ print("Print the total rainfall for the week.",round(total_rainfall,2))
 avg_rainfall=np.mean(sample_rainfall_np)
 print('Print the average rainfall for the week.',round(avg_rainfall,2))
 
-no_rain=np.where(sample_rainfall_np==0.0)
-count_no_rain=np.count_nonzero(sample_rainfall==0.0)
-print('how many days had no rain (0 mm)',count_no_rain,' days')
+no_rain=np.sum(sample_rainfall_np==0.0)
+print('how many days had no rain (0 mm)',no_rain,' days')
 
 
 more_5_Rain=np.where(sample_rainfall_days['rainn']>5)
+
 print("Days where rainfall was more than 5 mm is ", sample_rainfall_days[more_5_Rain],' days')
 
 
