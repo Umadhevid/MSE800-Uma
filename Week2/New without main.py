5270213594be686e61d125a70f69668699cd91fd
umadhevid <umadhevid@gmail.com>
2025-08-10 08:49:23 +1200

    without init

---
+++ Week2/New without main.py
@@ -0,0 +1,22 @@
+class StringManipulator:
+    # def __init__(self,text):
+    #     self.text=text
+
+    def find_character(name,char):
+        return name.find(char)
+    def string_length(strings):
+        return len(strings)
+    def convert(strings):
+        return strings.upper()
+
+def main():
+    name='sunDay'
+    result=StringManipulator.find_character(name,'a')
+    print('Character found in place',result)
+    print('String Length',StringManipulator.string_length(name))
+    print('String Converted in to Upper case',StringManipulator.convert(name))
+    
+
+if __name__=="__main__":
+    main()
+
--- Week2/new.py
+++ Week2/new.py
@@ -1,6 +1,7 @@
 class StringManipulator:
     def __init__(self,text):
         self.text=text
+
     def find_character(self,char):
         return self.text.find(char)
     def string_length(self):
