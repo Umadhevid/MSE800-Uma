76e7bb497f997a4830c1ec734c85b463a7b3749a
umadhevid <umadhevid@gmail.com>
2025-08-10 08:25:40 +1200

    Create new.py

---
+++ Week2/new.py
@@ -0,0 +1,17 @@
+class StringManipulator:
+    def __init__(self,text):
+        self.text=text
+    def find_character(self,char):
+        return self.text.find(char)
+    def string_length(self):
+        return len(self.text)
+    def convert(self):
+        return self.text.upper()
+name=StringManipulator("example")
+result=name.find_character('x')
+length=name.string_length()
+convert=name.convert()
+print(result)
+print(length)
+print(convert)
+# string's length and convert it to uppercase.
