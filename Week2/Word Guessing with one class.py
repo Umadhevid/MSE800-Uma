acb6661adc485b48df34b7fc1bdde73a7414c0b1
umadhevid <umadhevid@gmail.com>
2025-08-10 00:50:15 +1200

    Create Word Guessing with one class.py

---
+++ Week2/Word Guessing with one class.py
@@ -0,0 +1,46 @@
+import random
+class guess_game:
+    def generate_blank():
+        blank='_'*len(random_words)
+        print('Leaving total blank ',blank,len(random_words))
+        return
+
+    def checking_user_inputs():
+        
+        
+        letter = input("User please enter a single letter to guess the random word: ").strip() 
+        if letter in random_words:
+            total_letter.add(letter)
+            print('Letter found here')
+            print("total_letter:," ,total_letter)
+            result=''.join(c if c in total_letter else'_' for c in random_words )
+            print('see the letters', result)   
+            timeout=0     
+
+        else:
+            print('Sorry no letter matches here , Try Again')
+                
+        
+        return 
+
+
+if __name__ == "__main__":
+    words = ["colors", "dress", "laptops", "month", "crew", "product"]
+    random_words=random.choice(words)
+    total_letter=set()
+    timeout=0
+    guess_game.generate_blank()
+    while True:
+        guess_game.checking_user_inputs()
+        if timeout!=0 and timeout>5:
+            print('Game Over')
+            break
+        else:
+            timeout=1+timeout
+        if   total_letter==set(random_words) :
+            print("Found all letters, Congrats")
+            break
+        else:
+            continue
+    
+
