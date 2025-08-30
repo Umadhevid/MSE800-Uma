1d3828f93d6dd2e1eb94c72919503fe9c2ab278f
umadhevid <umadhevid@gmail.com>
2025-08-31 10:12:46 +1200

    Update act6_1.py

--- Week 5/act6_1.py
+++ Week 5/act6_1.py
@@ -6,6 +6,12 @@ class Student:
 
     def get_grade(self):
         return self.__grade
+    def set_grade(self, grade):
+        if isinstance(grade, str) and len(grade) > 0:
+            self.__grade = grade
+        else:
+            print("Invalid Grade. It must be a non-empty string.")
+
     def student_details(self):
         print("Student detials",self.name,self._age,self.__grade)
 class admin(Student):
@@ -15,6 +21,11 @@ class admin(Student):
         print("Admin student", self.name)
         print("Admin student age", self._age)
         print("Admin student grade", self.get_grade())# unable to directly access to grade, since its private we can access via get_grade
+    def updated_info(self):
+        if self.get_grade()=='A':
+            self.set_grade('A+')
+            print("Uodated student grade is :",self.get_grade())
+            
 
 
 s = Student('Ali', 20)
@@ -24,3 +35,4 @@ print(s.get_grade())  # correct way
 s.student_details()
 a=admin("Uma",22)
 a.admin_student()
+a.updated_info()
