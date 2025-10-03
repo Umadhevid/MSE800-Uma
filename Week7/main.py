cf610509ac00dfb32ad87c234128a3a2993eefee
umadhevid <umadhevid@gmail.com>
2025-10-04 09:29:38 +1300

    1

--- Week7/main.py
+++ Week7/main.py
@@ -1,13 +1,27 @@
-from Normal_DB import User_Serivce,Student_Serivce
 
-class Check_Db:
+"""Main module to check database services."""
+
+from Normal_DB import User_Serivce, Student_Serivce
+
+
+class CheckDb:
+    """Class to check user and student services."""
+
     def __init__(self):
-        self.self=self
-    def timecheck(self):
-        user_service=User_Serivce.search_user("Uma")
-        print("f UserService user list {user_service}")
-        stud_service=Student_Serivce.search_user("Dhevi")
-        print("f UserService user list {user_service}")
+        pass  # No need for self.self
 
-if __init__=="__main__":
-    Check_Db.timecheck()
+    def timecheck(self):
+        """Check and print user and student service results."""
+        user_service = User_Serivce.search_user("Uma")
+        print(f"UserService user list: {user_service}")
+
+        stud_service = Student_Serivce.search_user("Dhevi")
+        print(f"StudentService user list: {stud_service}")
+
+
+if __name__ == "__main__":
+    checker = CheckDb()
+    checker.timecheck()
+
+
+
